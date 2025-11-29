4412609f62575115ee3cd2e796190e2e0d15ce32
Mhaigh81 <mhaigh@me.com>
2025-11-29 13:59:37 -0500

    stuck in Java

---
+++ movies.js
@@ -0,0 +1,26 @@
+const moviesWrapper = document.querySelector(".movies")
+console.log(moviesWrapper)
+
+
+function searchChange(event){
+    // console.log(event.target.value)
+}
+
+async function renderMovies(){
+    const response = (await fetch('http://www.omdbapi.com/?s=batman&apikey=900cdde7'))
+    const data = await response.json()
+    const moviesArr = data.Search
+    console.log(moviesArr)
+    // moviesWrapper.innerHTML = moviesArr.map((movie) => {
+    //     return `
+    //     <div class="movie">
+    //     <img src= alt="" />
+    //     <h2>Title</h2>
+    //     <h4>Year</h4>
+    //     <button>Learn More</button>
+    //     </div>
+    //     `
+    // })
+}
+
+renderMovies();
